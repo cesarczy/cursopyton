24a6bfcb56ba9a6741ed9a7f36546165f8f4ea8f
erlansiqueira <cesarczy@gmail.com>
2021-07-09 19:33:25 -0300

    1

--- aulas/indices.py
+++ aulas/indices.py
@@ -8,13 +8,25 @@ nova_string = ''
 
 input_do_user = input('Digita um letra: ')
 
-while contador < tamanho_frase:
-    # print(frase[contador], contador) um print por execução
-    letra = frase[contador]
-    if letra == input_do_user:
-        nova_string += input_do_user.upper()
+# # Com while
+# while contador < tamanho_frase:
+#     # print(frase[contador], contador) um print por execução
+#     letra = frase[contador]
+#     if letra == input_do_user:
+#         nova_string += input_do_user.upper()
+#     else:
+#         nova_string += letra
+#     contador += 1
+
+# Com FOR
+
+for letra in frase:
+    if letra == 'r':
+        nova_string = nova_string + letra.upper()
+    elif letra == 't':
+        nova_string += letra.upper()
     else:
         nova_string += letra
-    contador += 1
+
 
 print(nova_string)
---
+++ aulas/listas.py
@@ -0,0 +1,4 @@
+# Listas
+# fatiamento
+# append, insert, pop, del, clear, extend, min, max, range
+
